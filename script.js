7c131a021405dab5589a87615be96211b9cc6775
tondaolzbut <tonda.olzbut@email.cz>
2025-04-22 21:21:19 +0200

    rough class architecture

--- script.js
+++ script.js
@@ -3,3 +3,31 @@ document.querySelectorAll("#sequencer button").forEach(btn => {
       btn.classList.toggle("pressed");
     });
   });
+
+class Sequencer{
+  tempo;
+  stepCount;
+  isPlaying;
+  nextStep(){  }
+  start(){}
+  stop(){}
+}
+class Step{
+  index;
+  pitch;
+  length;
+  velocity;
+  isActive;
+  Sample;
+  play(){}
+  clear(){}
+  put(){}
+  modify(){}
+}
+class Sample{
+  source;
+}
+class Track{
+  Sequencer;
+  isMuted;
+}
