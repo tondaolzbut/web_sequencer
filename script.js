e144a8178af24bbadfd2abcb36fe3f12d81eeda5
tondaolzbut <tonda.olzbut@email.cz>
2025-04-29 14:28:06 +0200

    Sequencer, Step constructors

--- script.js
+++ script.js
@@ -5,21 +5,49 @@ document.querySelectorAll("#sequencer button").forEach(btn => {
   });
 
 class Sequencer{
-  tempo;
-  stepCount;
-  isPlaying;
-  nextStep(){  }
-  start(){}
-  stop(){}
+  tempo = 120;
+  stepCount = 8;
+  isPlaying = false;
+  activeStep = 0;
+  steps = [];
+  constructor(stepCount){
+    this.stepCount = stepCount;
+    for (let i = 0; i < stepCount; i++) {
+      this.steps.push(new Step(i));
+    }
+  }
+  nextStep(){ 
+    if (this.activeStep > this.stepCount -2){
+      this.activeStep = 0;
+    }
+    else{
+      this.activeStep += 1;
+    }
+  }
+  start(){
+    this.isPlaying = true
+    while (this.isPlaying){
+      this.steps[this.activeStep].play();
+      this.nextStep();
+    }
+  }
+  stop(){
+    this.isPlaying = false
+  }
 }
 class Step{
   index;
   pitch;
   length;
   velocity;
-  isActive;
+  isActive = false;
   Sample;
-  play(){}
+  constructor(index){
+    this.index = index;
+  }
+  play(){
+    alert("playing step "+this.index);
+  }
   clear(){}
   put(){}
   modify(){}
@@ -31,3 +59,6 @@ class Track{
   Sequencer;
   isMuted;
 }
+
+seq = new Sequencer(8);
+seq.start();
